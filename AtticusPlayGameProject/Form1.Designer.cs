a5fe921f7e0f50e17da70bff55e7863af1654d26
AttiHaslSDSS <atticushaslam911@outlook.com>
2022-09-15 10:52:13 -0400

    Add project files.

---
+++ AtticusPlayGameProject/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AtticusPlayGameProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(265, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Video Game";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Firebrick;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(344, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Begin?";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(228, 127);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(374, 53);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Orange;
+            this.Name = "Form1";
+            this.Text = "Video Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ AtticusPlayGameProject/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace AtticusPlayGameProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        
+        {
+            outputLabel.Text = "Game will start in: 5";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = "Game will start in: 4";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = "Game will start in: 3";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = "Game will start in: 2";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = "Game will start in: 1";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = "Go!";
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkOrange;
+            Refresh();
+            Thread.Sleep(100);
+            BackColor = Color.DarkRed;
+            outputLabel.Text = "You Win! Good Job!";
+            BackColor = Color.Black;
+        } 
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
